d33a4ca6a94b61110e5ebe7d7d456a58524d1959
Bobsledbeau <beaubuys@gmail.com>
2026-02-28 21:34:57 +0000

    Fix rootPath for Render static serving

--- nodejs_space/src/app.module.ts
+++ nodejs_space/src/app.module.ts
@@ -1,28 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { ItemsModule } from './items/items.module';
-import { ThemesModule } from './themes/themes.module';
-import { StoriesModule } from './stories/stories.module';
-import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: '.env',
-    }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-      exclude: ['/api*', '/health*'],
-      serveRoot: '/',
+      rootPath: join(process.cwd(), '..', 'react_native_space', 'dist'),
     }),
-    ItemsModule,
-    ThemesModule,
-    StoriesModule,
   ],
-  controllers: [HealthController],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
