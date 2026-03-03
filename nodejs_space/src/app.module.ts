36ec575599a6e2c99df1af354809d4d49e9fea1a
Bobsledbeau <beaubuys@gmail.com>
2026-03-03 08:56:45 +0000

    Fix: register Items/Themes/Stories modules in AppModule

--- nodejs_space/src/app.module.ts
+++ nodejs_space/src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ItemsModule } from './items/items.module';
+import { ThemesModule } from './themes/themes.module';
+import { StoriesModule } from './stories/stories.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     ServeStaticModule.forRoot({
       rootPath: join(process.cwd(), '..', 'react_native_space', 'dist'),
+      exclude: ['/api*'],
     }),
+    ItemsModule,
+    ThemesModule,
+    StoriesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
